218d1d6558f548fc56e4f5791d4f4364ab204848
Shubh Gajjar <shubhgajjar2004@gmail.com>
2023-02-18 10:31:42 +0530

    form1

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask , render_template , request, url_for , redirect , jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/', methods=['GET','POST'])
+def home():
+     if request.method == "POST":
+       Title = request.form.get("Title")
+       story = request.form.get("story")
+       return "Your name is "+Title + story
+     return render_template("index.html")
  
 if __name__ == '__main__':
- 
     app.run()
