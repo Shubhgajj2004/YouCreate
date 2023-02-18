f30184572bbc220924fffa96342297a498f09a28
ved-2004 <ved29022004@gmail.com>
2023-02-18 11:28:17 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-import os
-from flask import Flask , render_template , request, url_for , redirect , jsonify
+from sapling import SaplingClient
+from flask import Flask , render_template , request
+
+API_KEY = 'NLDUU0EBQN7S97RNAW0AYT35FFMO3BNY'
+client = SaplingClient(api_key = API_KEY)
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def home():
-     if request.method == "POST":
-       Title = request.form.get("Title")
-       story = request.form.get("story")
-       return "Your name is "+Title + story
-     return render_template("index.html")
+    if request.method == 'POST':
+        original = request.form.get('story')
+        return result(original)
+    return render_template('index.html')
+
  
+@app.route('/result',methods=['GET','POST'])
+def result(original):
+    if request.method == "POST":
+        text = str(original)
+        edits = sorted(edits, key=lambda e: -1 * (e['sentence_start'] + e['start']))
+        for edit in edits:
+          start = edit['sentence_start'] + edit['start']
+          end = edit['sentence_start'] + edit['end']
+          # if start > len(text) or end > len(text):
+          #   print(f'Edit start:{start}/end:{end} outside of bounds of text:{text}')
+          #   continue
+          text = text[: start] + edit['replacement'] + text[end:]
+        return render_template('result.html',original = original,enhanced = text)
+
 if __name__ == '__main__':
     app.run()
